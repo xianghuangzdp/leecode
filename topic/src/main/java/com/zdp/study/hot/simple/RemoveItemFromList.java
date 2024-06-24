cd6a31a5026f9c6ab51ceefccf5a47311e0b6268
zdp <zdp_main@163.com>
2024-06-24 23:33:59 +0800

    移除元素

---
+++ topic/src/main/java/com/zdp/study/hot/simple/RemoveItemFromList.java
@@ -0,0 +1,92 @@
+package com.zdp.study.hot.simple;
+
+/**
+ * @author zdp
+ * @date 2024/6/24 23:05
+ * @desc
+ */
+public class RemoveItemFromList {
+
+    public static void main(String[] args) {
+        System.out.println(case1()?"case1 success":"case1 fail");
+        System.out.println(case2()?"case2 success":"case2 fail");
+        System.out.println(case3()?"case3 success":"case3 fail");
+    }
+
+    public static boolean case1(){
+        int[] nums1 ={3,2,2,3};
+        int value = 3;
+
+        int k = removeElement(nums1,value);
+
+        System.out.println(join(nums1));
+
+        return checkResult(nums1,k,value);
+    }
+
+    public static boolean case2(){
+        int[] nums1 ={0,1,2,2,3,0,4,2};
+        int value = 2;
+
+        int k = removeElement(nums1,value);
+
+        System.out.println(join(nums1));
+
+        return checkResult(nums1,k,value);
+    }
+
+    public static boolean case3(){
+        int[] nums1 ={4,5};
+        int value = 5;
+
+        int k = removeElement(nums1,value);
+
+        System.out.println(join(nums1));
+
+        return checkResult(nums1,k,value);
+    }
+
+    public static boolean checkResult(int [] nums1,int k,int value){
+        for (int i = 0; i < k; i++){
+            if (nums1[i] == value){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static int removeElement(int[] nums, int val){
+        int lastIndex = nums.length-1;
+        int count = 0;
+
+        for (int i = 0; i <= lastIndex;i++){
+            int value = nums[i];
+            if (value != val){
+                count++;
+                continue;
+            }
+
+            for (;lastIndex >= i; lastIndex--){
+                int lastValue = nums[lastIndex];
+                if (lastValue != val){
+                    nums[i] = lastValue;
+                    lastIndex--;
+                    count++;
+                    break;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private static String join(int[] nums){
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < nums.length;i++){
+            builder.append(nums[i]).append(",");
+        }
+
+        return builder.toString();
+    }
+}
